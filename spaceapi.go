d6edbef8bf32e5a7a448e5235fada590384728e4
Johannes Fürmann <johannes@weltraumpflege.org>
2014-09-20 23:44:16 +0200

    Split huge `SpaceAPI` Structures in small structs

--- spaceapi.go
+++ spaceapi.go
@@ -4,42 +4,48 @@ import "encoding/json"
 import "net/http"
 import "io/ioutil"
 
+type Location struct {
+	Address string  `json:"address"`
+	Lat     float32 `json:"lat"`
+	Lon     float32 `json:"lon"`
+}
+
+type State struct {
+	Open          bool   `json:"open"`
+	Lastchange    int32  `json:"lastchange"`
+	TriggerPerson string `json:"trigger_person"`
+	Message       string `json:"message"`
+	Icon          struct {
+		Open   string `json:"open"`
+		Closed string `json:"string"`
+	} `json:"icon"`
+}
+
+type Contact struct {
+	Phone    string `json:"phone"`
+	Sip      string `json:"sip"`
+	Irc      string `json:"irc"`
+	Twitter  string `json:"twitter"`
+	Facebook string `json:"facebook"`
+	Google   struct {
+		Plus string `json:"plus"`
+	} `json:"google"`
+	Identica   string `json:"identica"`
+	Foursquare string `json:"foursquare"`
+	Email      string `json:"email"`
+	Ml         string `json:"ml"`
+	Jabber     string `json:"jabber"`
+	IssueMail  string `json:"issue_mail"`
+}
+
 type SpaceAPI struct {
-	Api      string `json:"api"`
-	Space    string `json:"space"`
-	Logo     string `json:"logo"`
-	Url      string `json:"url"`
-	Location struct {
-		Address string  `json:"address"`
-		Lat     float32 `json:"lat"`
-		Lon     float32 `json:"lon"`
-	} `json:"location"`
-	State struct {
-		Open          bool   `json:"open"`
-		Lastchange    int32  `json:"lastchange"`
-		TriggerPerson string `json:"trigger_person"`
-		Message       string `json:"message"`
-		Icon          struct {
-			Open   string `json:"open"`
-			Closed string `json:"string"`
-		} `json:"icon"`
-	} `json:"state"`
-	Contact struct {
-		Phone    string `json:"phone"`
-		Sip      string `json:"sip"`
-		Irc      string `json:"irc"`
-		Twitter  string `json:"twitter"`
-		Facebook string `json:"facebook"`
-		Google   struct {
-			Plus string `json:"plus"`
-		} `json:"google"`
-		Identica   string `json:"identica"`
-		Foursquare string `json:"foursquare"`
-		Email      string `json:"email"`
-		Ml         string `json:"ml"`
-		Jabber     string `json:"jabber"`
-		IssueMail  string `json:"issue_mail"`
-	} `json:"contact"`
+	Api                 string                 `json:"api"`
+	Space               string                 `json:"space"`
+	Logo                string                 `json:"logo"`
+	Url                 string                 `json:"url"`
+	Location            Location               `json:"location"`
+	State               State                  `json:"state"`
+	Contact             Contact                `json:"contact"`
 	IssueReportChannels []string               `json:"issue_report_channels"`
 	Sensors             map[string]interface{} `json:"sensors"`
 	Feeds               map[string]struct {
